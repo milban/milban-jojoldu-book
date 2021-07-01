8bba07042ac222767fb8451a53853996a9fb16bc
JunHyeock Park <wnsgur6311@gmail.com>
2021-07-01 20:44:42 +0900

    구글 인증

---
+++ src/main/java/com/milban/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.milban.book.springboot.config.auth.dto;
+
+import com.milban.book.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
